7bf763bc303e098af3f1c2aa7c00dfe8fcbb8c6f
immeo <wayfarer245@gmail.com>
2024-10-29 02:12:08 +0300

    21-add remove favorite books logic

---
+++ src/components/Books/BooksFavorites/BooksFavorites.props.ts
@@ -0,0 +1,5 @@
+export interface IBook {
+	cover_image_path: string;
+	title_books: string;
+	book_slug: string;
+}
--- src/components/Books/BooksFavorites/BooksFavorites.tsx
+++ src/components/Books/BooksFavorites/BooksFavorites.tsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-
-interface IBook {
-	cover_image_path: string;
-	title_books: string;
-	book_slug: string;
-}
+import FavoriteSvgIcon from '../../../assets/icons/FavoriteSvgIcon';
+import { removeFavorite } from '../../../store/favorite.slice';
+import { IBook } from './BooksFavorites.props';
 
 function BooksFavorites() {
-	const data = localStorage.getItem('favorites');
-	const favorites: IBook[] = data ? JSON.parse(data) : [];
+	const dispatch = useDispatch();
+	const [favorites, setFavorites] = useState<IBook[]>([]);
+
+	useEffect(() => {
+		const data = localStorage.getItem('favorites');
+		const localFavorites: IBook[] = data ? JSON.parse(data) : [];
+		setFavorites(localFavorites);
+	}, []);
+	const onFavorite = (book: IBook) => {
+		dispatch(removeFavorite(book.book_slug));
+		const updatedFavorites = favorites.filter(
+			b => b.book_slug !== book.book_slug
+		);
+		setFavorites(updatedFavorites);
+		localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+	};
 
 	return (
 		<div className='flex flex-wrap items-stretch justify-center gap-5'>
+			{favorites.length === 0 && (
+				<p className='text-center text-2xl'>У вас нет избранных книг</p>
+			)}
 			{favorites.map(book => (
 				<div
 					key={book.cover_image_path}
@@ -23,13 +39,22 @@ function BooksFavorites() {
 							src={book.cover_image_path}
 							alt={book.title_books}
 						/>
+						<button
+							type='button'
+							className='relative'
+							onClick={() => onFavorite(book)}
+						>
+							<span className='absolute bottom-80 left-80'>
+								<FavoriteSvgIcon color='red' />
+							</span>
+						</button>
 					</div>
 					<div className='flex-grow w-full text-center'>
 						<p>{book.title_books}</p>
 					</div>
 					<div className='flex items-center justify-center'>
 						<Link
-							className='align-middle hover:hover-bg-action mt-3 rounded-lg bg-bg-action px-2 py-2 font-bold text-main-color shadow-md duration-100 hover:text-hover-main-color sm:mt-0 md:px-6 md:py-3 md:text-lg'
+							className='align-middle hover:bg-action mt-3 rounded-lg bg-bg-action px-2 py-2 font-bold text-main-color shadow-md duration-100 hover:text-hover-main-color sm:mt-0 md:px-6 md:py-3 md:text-lg'
 							to={`/book/${book.book_slug}`}
 						>
 							О книге
